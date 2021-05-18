78ff3cb2c836b6cde82f1967722e0ba438f3ea4d
Pat <patrickxavierrichard@gmail.com>
2021-05-18 22:18:50 +0200

    assoc starts

--- priv/repo/migrations/20210518123831_create_flashcards.exs
+++ priv/repo/migrations/20210518123831_create_flashcards.exs
@@ -4,6 +4,14 @@ defmodule Furansulive.Repo.Migrations.CreateFlashcards do
   def change do
     create table(:flashcards) do
       add :name, :string
+      add :target_language, :string
+      add :target_language_pronunciation, :string
+      add :target_language_sound_file_path, :string
+      add :target_language_meaning, :string
+      add :source_language_meaning, :string
+      add :target_language_picture_file_path, :string
+      add :target_language_notes, :string
+      add :flashcard_type, :integer
 
       timestamps()
     end
---
+++ priv/repo/migrations/20210518201709_flashcard_belongs_to_user.exs
@@ -0,0 +1,7 @@
+defmodule Furansulive.Repo.Migrations.FlashcardBelongsToUser do
+  use Ecto.Migration
+
+  def change do
+
+  end
+end
