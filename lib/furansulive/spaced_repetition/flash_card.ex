d4f72f51386d3ee9eea123dbfb9aa4dc7187c5c3
Patrick <prichard@herculepro.com>
2021-05-18 15:08:23 +0200

    relations word/flashcard

--- lib/furansulive/spaced_repetition/flash_card.ex
+++ lib/furansulive/spaced_repetition/flash_card.ex
@@ -2,8 +2,16 @@ defmodule Furansulive.SpacedRepetition.FlashCard do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Furansulive.Directory.Word
+
   schema "flashcards" do
-    field :name, :string
+    field :target_expression, :string
+    field :target_expression_pronunciation, :string
+    field :target_expression_sound_file_path, :string
+    field :meaning, :string
+    field :picture_file_path, :string
+    field :notes, :string
+    has_one :word, Word
 
     timestamps()
   end
